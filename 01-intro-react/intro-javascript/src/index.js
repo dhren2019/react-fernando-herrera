652d6aeb3aee71abea76d39a71d41a4381d1664a
Rodrigo Glodosindo <dhren@MacBook-Pro.home>
2021-02-08 19:11:14 +0100

    COMMIT

--- 01-intro-react/intro-javascript/src/index.js
+++ 01-intro-react/intro-javascript/src/index.js
@@ -1,24 +1,6 @@
-//const nombre = 'Rodrigo';
-//const apellido = 'Glodossindo';
-//
-//const getUsuario = (nombre) => ({
-//    uid: 'jnjwjd',
-//    userName: nombre
-//
-//})
-//
-//console.log(getUsuario)
 
+const promesa = new Promise();
 
-//DESTRUCTURING
+const getHeroe = ( id ) => {
 
-const persona = {
-    nombre: 'Tony',
-    edad: 45,
-    clave: 'Ironman'
 }
-
-const { nombre, clave } = persona;
-console.log(nombre
-)
-

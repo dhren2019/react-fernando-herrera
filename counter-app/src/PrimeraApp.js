b33bfa3660bd471f0824efb34b2bb080f277e1cb
Rodrigo Glodosindo <dhren@MacBook-Pro.home>
2021-02-08 21:00:24 +0100

    Commit props

---
+++ counter-app/src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const PrimeraApp = ( props) => {
+
+    const saludo = 'Hola mundo';
+    return (
+        <>
+            <h1>{ saludo }</h1>
+            <p>Mi primera app</p>
+        </>
+    )
+}
+export default PrimeraApp;

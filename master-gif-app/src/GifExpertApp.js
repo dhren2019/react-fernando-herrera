eefd143d56aed1e863821327633d2022b0c67b96
dhren2019 <roglobar10@gmail.com>
2021-02-10 20:45:03 +0100

    Fetch API class

---
+++ master-gif-app/src/GifExpertApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Dragon Ball', 'Samurai X'];
+    const [categories, setcategories] = useState([ 'Dragon Ball'])
+
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setcategories={ setcategories } />
+            <hr />
+
+           
+            <ol>
+                {
+                    categories.map( category => {
+                        <GifGrid 
+                        key={ category}
+                        category={ category} />
+                    })
+                }
+            </ol>
+        </>
+    )
+}
---
+++ master-gif-app/src/components/AddCategory.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ( {setcategories }) => {
+
+    const [inputValue, setinputValue] = useState(['']);
+    const handleInputChange = (e) => {
+        setinputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        if ( inputValue.trimEnd().length > 2) {
+            setcategories( cats => [...cats, inputValue]);
+            setinputValue('');
+        }
+        console.log('submit hechor')
+    }
+    return (
+        <>
+            <form onSubmit={ handleSubmit}>
+                <input
+
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+
+                />
+            </form>
+        </>
+    )
+}
+
+
+AddCategory.propTypes = {
+    setcategories: PropTypes.func.isRequired
+}
